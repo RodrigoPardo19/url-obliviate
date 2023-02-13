af9c8e43d9d18ca2cec818fa63a1514373eabe11
Rodrigo Pardo <44351158+RodrigoPardo19@users.noreply.github.com>
2023-02-13 00:19:55 -0300

    add CD pipeline
    * add lint step to pipeline
    
    * add CD pipeline
    
    * fix cd pipeline file
    use of tabs is changed to spaces
    
    * fix cd pipeline file, remove merge event
    
    * fix tailwind files to lint step in CI

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import("tailwindcss").Config} */
+/* eslint-env node */
+module.exports = {
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				'roboto': ['Roboto'],
+			},
+			colors: {
+				primary: '#ddff55',
+			},
+		},
+	},
+	plugins: [],
+};
