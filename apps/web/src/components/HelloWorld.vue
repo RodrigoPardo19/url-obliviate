c11cfcea61f07e07782806b07a016ccd5e0e620d
RodrigoPardo19 <rodrigo01alexis@gmail.com>
2023-02-24 23:19:07 -0300

    fix use of tabs in docker-compose

--- apps/web/src/components/HelloWorld.vue
+++ apps/web/src/components/HelloWorld.vue
@@ -11,7 +11,8 @@ defineProps<{ msg: string }>();
 		<div class="bg-primary py-4 px-10 rounded-xl text-center">
 			<h1 class="font-bold text-7xl text-black">{{ msg }}</h1>
 		</div>
-		<p class="text-xl mt-2">A simple web to rembember
+		<p class="text-xl mt-2">
+			A simple web to remember
 			<span class="bg-black rounded-xl px-2 text-primary font-medium">URLS</span>
 		</p>
 	</div>
